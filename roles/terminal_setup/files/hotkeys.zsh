31fe470d3a8b378e0acfb94858cf010a24ae3d2c
Waldemar <waldemar13311@gmail.com>
2026-06-08 23:16:04 +0300

    Some fixed

--- roles/terminal_setup/files/hotkeys.zsh
+++ roles/terminal_setup/files/hotkeys.zsh
@@ -1,3 +1,4 @@
 # --- Хоткеи ---
-bindkey '^r' fzf-insert-history # fzf-insert-history по нажатию Ctrl + R (быстрый поиск команд по истории)
-bindkey '^t' fzf-insert-files   # fzf-insert-files по нажатию Ctrl + T (быстрый поиск файлов)
+bindkey '^r' fzf-insert-history     # fzf-insert-history по нажатию Ctrl + R (быстрый поиск команд по истории)
+bindkey '^t' fzf-insert-files       # fzf-insert-files по нажатию Ctrl + T (быстрый поиск файлов)
+bindkey '^W' vi-backward-kill-word  # Ctrl + T как в редакторе vi
