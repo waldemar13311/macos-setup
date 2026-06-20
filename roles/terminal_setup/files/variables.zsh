071804b53e13cef5bc00dc0e033863c83df5498a
Waldemar <waldemar13311@gmail.com>
2026-06-20 19:51:32 +0300

    Added new setup point

--- roles/terminal_setup/files/variables.zsh
+++ roles/terminal_setup/files/variables.zsh
@@ -8,5 +8,9 @@ export PATH="$HOME/.local/bin:$PATH"
 # curl из homebrew, так как стандартный mac-овский не удобный
 export PATH="/opt/homebrew/opt/curl/bin:$PATH"
 
+# Зеркала для tenv
+export TENV_TERRAFORM_REMOTE="https://hashicorp-releases.yandexcloud.net"
+export TENV_OPENTOFU_REMOTE="https://github.com/opentofu/opentofu/releases/download"
+
 setopt NOBEEP               # Убрать звуки терминала
 setopt NUMERIC_GLOB_SORT    # Умная сортировка файлов с цифрами (file10 после file9, не после file1)
